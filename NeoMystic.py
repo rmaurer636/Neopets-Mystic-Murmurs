addfa71becff77d30b3ea121da0f356548a110c4
minicoz <minicoz12@gmail.com>
2017-11-18 23:44:07 -0600

    New updated files
    Works a little better

--- NeoMystic.py
+++ NeoMystic.py
@@ -3,14 +3,14 @@
 
 # ### Neopets Mystic Murmuers Nov 2017
 
-# In[106]:
+# In[166]:
 
 import csv
 
 
 # We open each textfile in my folder with the csv.reader() function, then we save each list into three different lists in python for analysis.
 
-# In[115]:
+# In[184]:
 
 tex1, text2, text3 = [], [], []
 
@@ -27,10 +27,11 @@ with open('text3.txt') as inputfile:
 
 # One we have our lists, lets analyze list 1 with list 2 and get the common characters at the same index. Then we do the same with list 1 and list 3.
 
-# In[116]:
+# In[185]:
 
 joined_text1 = []
 joined_text2 = []
+joined_text3 = []
 
 for i in range(len(text1)):
     if text1[i] == text2[i]:
@@ -38,14 +39,22 @@ for i in range(len(text1)):
 for i in range(len(text1)):
     if text1[i] == text3[i]:
         joined_text2.append(text3[i])
+for i in range(len(text1)):
+    if text2[i] == text3[i]:
+        joined_text3.append(text2[i])
 
 
 # Once we have our joined lists, I perform an "and" operation which only joins if the two inputs are the same. EX: a and b = 0, a and a = a
+# 
+# # Here is you answer
 
-# In[117]:
+# In[187]:
 
-print joined_text1, joined_text2
+a = joined_text1
+b = joined_text2
+c = joined_text3
+d = [x for x in a if x in b]
 
-result = joined_text1 and joined_text2
-print ''.join(result)
+
+print ''.join([x for x in d if x in c])
 
